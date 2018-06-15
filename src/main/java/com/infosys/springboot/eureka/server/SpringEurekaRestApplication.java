0efe1e4ae4f8ca5c57334694652852364cbf8f89
vikramadityasen <sen.vikramadityasen>
2018-06-15 21:14:56 +0530

    my first commit

---
+++ src/main/java/com/infosys/springboot/eureka/server/SpringEurekaRestApplication.java
@@ -0,0 +1,14 @@
+package com.infosys.springboot.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringEurekaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEurekaRestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/infosys/springboot/Eureka/springeurekarestapplication/SpringEurekaRestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.infosys.springboot.Eureka.springeurekarestapplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringEurekaRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
