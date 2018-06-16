0ddbcda3ec8f13d829ce7777416f63508980c321
vikramadityasen <sen.vikramadityasen>
2018-06-16 22:49:04 +0530

    commit for creditScore eureka client

---
+++ src/main/java/com/infosys/springboot/eureka/creditScore/creditScoreApplication/CreditScoreApplication.java
@@ -0,0 +1,12 @@
+package com.infosys.springboot.eureka.creditScore.creditScoreApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditScoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditScoreApplication.class, args);
+	}
+}
---
+++ src/test/java/com/infosys/springboot/eureka/creditScore/creditScoreApplication/CreditScoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.infosys.springboot.eureka.creditScore.creditScoreApplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CreditScoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
