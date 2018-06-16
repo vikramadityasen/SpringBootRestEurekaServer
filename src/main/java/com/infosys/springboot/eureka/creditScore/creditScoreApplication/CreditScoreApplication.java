9b5753af207d751b655e4cfb84160b6a92d581ae
vikramadityasen <sen.vikramadityasen>
2018-06-16 22:59:47 +0530

    added application.yml file

--- src/main/java/com/infosys/springboot/eureka/creditScore/creditScoreApplication/CreditScoreApplication.java
+++ src/main/java/com/infosys/springboot/eureka/creditScore/creditScoreApplication/CreditScoreApplication.java
@@ -2,8 +2,10 @@ package com.infosys.springboot.eureka.creditScore.creditScoreApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CreditScoreApplication {
 
 	public static void main(String[] args) {
