122c06e2989b91fafc0a7eb4b244db33ad8c11a6
vikramadityasen <sen.vikramadityasen>
2018-06-19 21:47:44 +0530

    Initial commit for stub

---
+++ src/main/java/com/infosys/springboot/eureka/creditscorestubservice/CreditScoreStubApplication.java
@@ -0,0 +1,12 @@
+package com.infosys.springboot.eureka.creditscorestubservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditScoreStubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditScoreStubApplication.class, args);
+	}
+}
---
+++ src/test/java/com/infosys/springboot/eureka/creditScoreStub/creditScoreStubApplication/CreditScoreStubApplicationTests.java
@@ -0,0 +1,16 @@
+package com.infosys.springboot.eureka.creditScoreStub.creditScoreStubApplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CreditScoreStubApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
